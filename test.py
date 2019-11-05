c3261bdd7c4cb0d227fc6a796ed9b870da9382ad
Cormac O'Brien <cormac@c-obrien.org>
2019-11-05 01:40:05 -0600

    Update test harness to specify expected status codes

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import os
 import requests
 import yaml
 
+METHODS = ['post', 'get', 'delete']
+
 dotenv.load_dotenv()
 
 d = os.getenv("DOMAIN")
@@ -19,19 +21,33 @@ else:
 
 URL = 'http://{}:{}/api'.format(DOMAIN, PORT)
 
+def validate_request(req):
+    if req['method'] is None or req['method'] not in METHODS:
+        print('Request has invalid method or method is None:\n{}'.format(req))
+        return False
+
+def check_status(req, resp):
+    if resp.status_code != req['response']['status']:
+        print('Unexpected status code for request (expected {}, got {}): {}'
+              .format(req['response']['status'], resp.status_code, req))
+        exit(1)
+
 with open('test-data.yaml') as yaml_file:
     test_data = yaml.load(yaml_file)
 
-print(test_data)
+reqs = test_data['requests']
+for req in reqs:
+    validate_request(req)
+    method = req['method'].lower()
+    full_url = URL + req['route']
+    if method == 'post':
+        resp = requests.post(full_url, json=req['body'])
+    elif method == 'get':
+        resp = requests.get(full_url)
+    elif method == 'delete':
+        resp = requests.delete(full_url)
+    else:
+        print('Method ({}) not recognized!'.format(method))
+        exit(1)
+    check_status(req, resp)
 
-teams = test_data['teams']
-for t in teams:
-    print('creating team: {}'.format(t))
-    r = requests.post(URL + '/teams', json=t)
-    r.raise_for_status()
-
-matches = test_data['matches']
-for m in matches:
-    print('creating match: {}'.format(m))
-    r = requests.post(URL + '/matches', json=m)
-    r.raise_for_status()
